53f6812d18ff8e4b7a3b59a540d4e1fc645e025f
Eliyhu Chitrit <eliyhuc@college.labs>
2024-01-14 09:09:25 +0200

    first initial

---
+++ src/screens/Catalog/Catalog.js
@@ -0,0 +1,13 @@
+import react from "react";
+import {View,Text} from 'react-native'
+import styles from '../../services/appStyle'
+
+const Catalog = () => {
+    return(
+        <View style={styles.container}>
+            <Text>Catalog</Text>
+        </View>
+    )
+}
+
+export default Catalog;
---
+++ src/screens/Support/Support.js
@@ -0,0 +1,13 @@
+import react from "react";
+import {View,Text} from 'react-native'
+import styles from '../../services/appStyle'
+
+const Support = () => {
+    return(
+        <View style={styles.container}>
+            <Text>Support</Text>
+        </View>
+    )
+}
+
+export default Support;
